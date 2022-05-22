631464d426be7c6aaf504fd6f23197bf5e3f96bf
DESKTOP-BVUTE1A\fumiy <fumiya0903613@gmail.com>
2022-05-22 12:49:44 +0900

    webpack周りの調整

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,20 +2,22 @@ const mix = require('laravel-mix');
 const glob = require('glob');
 
 glob.sync('resources/sass/*.scss').map(function (file) {
-		mix.sass(file, 'public/assets/css');
+	mix.sass(file, 'public/assets/css');
 });
 
 glob.sync('resources/js/*.js').map(function (file) {
-		mix.js(file, 'public/assets/js');
+	mix.js(file, 'public/assets/js');
 });
 
-mix.webpackConfig({
-    module: {
-        rules: [
-            {
-                test: /\.scss/,
-                loader: 'import-glob-loader'
-            },
-        ]
-    }
-})
+if (!mix.inProduction()) {
+    mix.webpackConfig({
+        module: {
+            rules: [
+                {
+                    test: /\.scss/,
+                    loader: 'import-glob-loader'
+                },
+            ]
+        }
+    })
+}
